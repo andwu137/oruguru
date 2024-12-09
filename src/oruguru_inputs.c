8fe448494b41c08b06c76d96e9dc78742022d5fb
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-12-09 18:04:16 -0500

    style(clang-format): apply clang format

--- src/oruguru_inputs.c
+++ src/oruguru_inputs.c
@@ -6,14 +6,20 @@
 #include <stdlib.h>
 
 /* PROTOTYPES */
-void draw_inputs(int input_flags);
-void input_map_read(struct InputMap *inputMap, long long *inputFlags);
-void input_map_init(size_t initCap, struct InputMap *inputMap);
-void input_map_default(size_t initCap, struct InputMap *inputMap);
-void input_map_push(struct InputMap *inputMap, struct InputMapping im);
+void
+draw_inputs(int input_flags);
+void
+input_map_read(struct InputMap *inputMap, long long *inputFlags);
+void
+input_map_init(size_t initCap, struct InputMap *inputMap);
+void
+input_map_default(size_t initCap, struct InputMap *inputMap);
+void
+input_map_push(struct InputMap *inputMap, struct InputMapping im);
 
 /* FUNCTIONS */
-void input_map_init(size_t initCap, struct InputMap *inputMap) {
+void
+input_map_init(size_t initCap, struct InputMap *inputMap) {
   inputMap->size = 0, inputMap->capacity = initCap,
   inputMap->map = calloc(initCap, sizeof(*inputMap->map));
   if (inputMap->map == NULL) {
@@ -22,7 +28,8 @@ void input_map_init(size_t initCap, struct InputMap *inputMap) {
   }
 }
 
-void input_map_default(size_t initCap, struct InputMap *inputMap) {
+void
+input_map_default(size_t initCap, struct InputMap *inputMap) {
   input_map_init(1 << 4, inputMap);
   {
     input_map_push(inputMap,
@@ -33,12 +40,14 @@ void input_map_default(size_t initCap, struct InputMap *inputMap) {
                    (struct InputMapping){.key = KEY_S, .input = INPUT_DOWN});
     input_map_push(inputMap,
                    (struct InputMapping){.key = KEY_D, .input = INPUT_RIGHT});
-    input_map_push(inputMap, (struct InputMapping){.key = KEY_SPACE,
-                                                   .input = INPUT_SELECT});
+    input_map_push(
+        inputMap,
+        (struct InputMapping){.key = KEY_SPACE, .input = INPUT_SELECT});
   }
 }
 
-void input_map_push(struct InputMap *inputMap, struct InputMapping im) {
+void
+input_map_push(struct InputMap *inputMap, struct InputMapping im) {
   if (inputMap->size == inputMap->capacity) {
     inputMap->capacity <<= 1;
     inputMap->map =
@@ -52,7 +61,8 @@ void input_map_push(struct InputMap *inputMap, struct InputMapping im) {
   inputMap->map[inputMap->size++] = im;
 }
 
-void input_map_read(struct InputMap *inputMap, long long *inputFlags) {
+void
+input_map_read(struct InputMap *inputMap, long long *inputFlags) {
   *inputFlags = 0;
   for (size_t i = 0; i < inputMap->size; i++) {
     if (IsKeyPressed(inputMap->map[i].key)) {
@@ -61,7 +71,8 @@ void input_map_read(struct InputMap *inputMap, long long *inputFlags) {
   }
 }
 
-void input_flags_debug_draw(int inputFlags) {
+void
+input_flags_debug_draw(int inputFlags) {
   float radius = 10;
   int x = 0;
   int y = 0;
