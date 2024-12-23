dfc620af7fc9d796acd1b926b4aa9708799bed3b
dbcalitis <daria_calitis@yahoo.com>
2024-12-22 21:28:59 -0500

    feat(drawing): created health

--- src/oruguru.c
+++ src/oruguru.c
@@ -1,30 +1,35 @@
 #include <raylib.h>
 #include <raymath.h>
 
-typedef struct Effect {
-  Vector2 vector;
-  int innerRadius;
-  int outerRadius;
-  char exist;
-} Effect;
+void draw_heart(Vector2 center, int radius, Color color) {
+  Vector2 circles[2] = {{.x = center.x - radius, .y = center.y - radius},
+                        {.x = center.x + radius, .y = center.y - radius}};
+  Vector2 triangle[3] = {
+      {.x = center.x + 2 * radius, .y = center.y - (radius * 0.62)},
+      {.x = center.x - 2 * radius, .y = center.y - (radius * 0.62)},
+      {.x = center.x, .y = center.y + radius * 1.5}};
 
-void spawnEffect(Effect *e, Vector2 v) {
-  e->vector = v;
-  e->innerRadius = 5;
-  e->outerRadius = 10;
-  e->exist = 1;
+  DrawCircle(circles[0].x, circles[0].y, radius, color);
+  DrawCircle(circles[1].x, circles[1].y, radius, color);
+  DrawTriangle(triangle[0], triangle[1], triangle[2], color);
+  DrawRectangle(center.x - radius / 2, center.y - radius, radius, radius,
+                color);
 }
 
-void drawEffect(Effect *e) {
-    if (e->exist) {
-      DrawRing(e->vector, e->innerRadius, e->outerRadius, 0, 360, 0, YELLOW);
-    }
-    e->innerRadius += 5;
-    e->outerRadius += 4;
+void draw_health(Vector2 topLeft, int totalHearts, int heartsLeft) {
+  int margin = 5;
+  int radius = 10; // HEART SIZE
+  Vector2 center = {.x = topLeft.x + margin,
+                    .y = topLeft.y + radius / 2 + margin};
 
-    if (e->innerRadius >= e->outerRadius) {
-      e->exist = 0;
+  for (int i = 0; i < totalHearts; i++) {
+    center.x = center.x + 4.5 * radius;
+    if (i < heartsLeft) {
+      draw_heart(center, radius, RED);
+    } else {
+      draw_heart(center, radius, GRAY);
     }
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -36,24 +41,16 @@ int main(int argc, char *argv[]) {
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
-  Vector2 v = {100, 100};
-  Effect effect;
-  spawnEffect(&effect, v);
     // Update
-
+    Vector2 v = {.x = 100, .y = 100};
     // Draw
     BeginDrawing();
     {
-      ClearBackground(RAYWHITE);
       ClearBackground(DARKGRAY);
       DrawText("oruguru", 190, 200, 20, LIGHTGRAY);
-      drawEffect(&effect);
+      draw_health(v, 3, 3);
     }
     EndDrawing();
-    if (effect.exist == 0) {
-      effect.exist = 1;
-      spawnEffect(&effect, v);
-    }
   }
 
   CloseWindow();
